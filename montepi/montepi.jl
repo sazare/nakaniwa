51b719b095406b6e79c6a1012e824384a4300f70
sazare <song.of.sand@gmail.com>
2019-11-27 20:11:01 +0900

    add montecalro pi sample
    	new file:   montepi.jl

---
+++ montepi/montepi.jl
@@ -0,0 +1,56 @@
+## 参照: https://qiita.com/boxboxbax/items/337a898766f643b1ab91
+
+function montepif(nums)
+ xs = rand(nums)
+ ys = rand(nums)
+
+ incirc = 0
+
+ for i in 1:nums
+   if xs[i]*xs[i] + ys[i]*ys[i] < 1.0
+     incirc += 1
+   end
+ end
+
+ montepi = 4 * incirc / nums
+
+ return montepi
+end
+
+function montepim(nums)
+ xs = rand(nums)
+ ys = rand(nums)
+
+ incirc = 0
+
+ map((x,y) -> begin
+     if x*x + y*y <1;
+       incirc += 1
+     end
+     end, xs, ys)
+
+ montepi = 4 * incirc / nums
+
+ return montepi
+end
+
+@info :for
+GC.gc()
+@time println(montepif(100000))
+GC.gc()
+@time println(montepif(1000000))
+GC.gc()
+@time println(montepif(10000000))
+GC.gc()
+@time println(montepif(100000000))
+
+@info map
+GC.gc()
+@time println(montepim(100000))
+GC.gc()
+@time println(montepim(1000000))
+GC.gc()
+@time println(montepim(10000000))
+GC.gc()
+@time println(montepim(100000000))
+
